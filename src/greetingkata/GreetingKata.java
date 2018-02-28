60e95ef37a6018569102844b6cc1718716ba014d
Ágoston Németh <nemagost@gmail.com>
2018-02-28 10:01:30 +0100

    greeting step 9

--- src/greetingkata/GreetingKata.java
+++ src/greetingkata/GreetingKata.java
@@ -3,42 +3,48 @@ package greetingkata;
 public class GreetingKata {
 
 	public static void main(String[] args) {
-		
-		System.out.println(border(greet("easter", "Sasha"), 80));
-		
+
+		System.out.println(border(greet("easter"), 80));
+
 	}
-	
+
 	public static String border(String message, int size) {
-		
+
 		String border = "";
-		
-		for(int i = 0; i < size; i++) {
+
+		for (int i = 0; i < size; i++) {
 			border += "*";
 		}
-		
+
 		return border + "\n" + message + "\n" + border;
-		
+
 	}
-	
+
+	public static String greet(String greeting) {
+
+		return greet(greeting, "my friend");
+
+	}
+
 	public static String greet(String greeting, String name) {
-		
-		switch(greeting) {
-		
+
+		switch (greeting) {
+
 		case "morning":
 			greeting = "Good morning";
 			break;
-			
+
 		case "easter":
 			greeting = "Happy Easter";
 			break;
-			
+
 		default:
 			break;
-		
+
 		}
-		
+
 		return String.format("%s, %s!", greeting, name);
-		
+
 	}
 
 }
