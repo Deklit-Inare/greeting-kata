d903d6495ff97b67d87e91abfa4c0c45a128ce7d
Ágoston Németh <nemagost@gmail.com>
2018-02-28 09:51:29 +0100

    greeting step 6

--- src/greetingkata/GreetingKata.java
+++ src/greetingkata/GreetingKata.java
@@ -4,7 +4,13 @@ public class GreetingKata {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Good morning!");
+		System.out.println(greet("Sasha") + "\n" + greet("Pacal"));
+		
+	}
+	
+	public static String greet(String name) {
+		
+		return String.format("Good morning, %s!", name);
 		
 	}
 
